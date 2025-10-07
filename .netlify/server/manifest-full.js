723039e698b954ddb5a13ed483aed42bb576344d
Johan Jönsson <jonsson.johan@outlook.com>
2025-10-07 18:18:15 +0200

    Fixed build error

---
+++ .netlify/server/manifest-full.js
@@ -0,0 +1,39 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["necromunda-bg.png","necromunda-frame.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {start:"_app/immutable/entry/start.DG2HjBlh.js",app:"_app/immutable/entry/app.DoLHsXDW.js",imports:["_app/immutable/entry/start.DG2HjBlh.js","_app/immutable/chunks/IqG0Puwh.js","_app/immutable/chunks/CAYq3aLb.js","_app/immutable/chunks/DNgd5NhE.js","_app/immutable/entry/app.DoLHsXDW.js","_app/immutable/chunks/DNgd5NhE.js","_app/immutable/chunks/CAYq3aLb.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/DFAwZzEG.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		remotes: {
+			
+		},
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		prerendered_routes: new Set([]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
---
+++ .netlify/server/nodes/0.js
@@ -0,0 +1,8 @@
+
+
+export const index = 0;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/0.C9yQlug_.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/BZSw608X.js","_app/immutable/chunks/DNgd5NhE.js"];
+export const stylesheets = ["_app/immutable/assets/0.D02XdO50.css"];
+export const fonts = [];
