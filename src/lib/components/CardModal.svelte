fbac4d9fe8724a4b5b2b3c625304291163e12e5b
Johan Jönsson <jonsson.johan@outlook.com>
2025-10-07 13:57:43 +0200

    Update CardModal.svelte

--- src/lib/components/CardModal.svelte
+++ src/lib/components/CardModal.svelte
@@ -99,8 +99,8 @@
     width: 100%;
     height: 80px;
     background-image: url('/necromunda-frame.png');
-    background-size: 100% 100%;
-    background-position: top center;
+    background-size: cover;
+    background-position: center;
     background-repeat: no-repeat;
     display: flex;
     align-items: center;
@@ -219,8 +219,8 @@
     width: 100%;
     height: 60px;
     background-image: url('/necromunda-frame.png');
-    background-size: 100% 100%;
-    background-position: bottom center;
+    background-size: cover;
+    background-position: center;
     background-repeat: no-repeat;
     transform: rotate(180deg);
     flex-shrink: 0;
